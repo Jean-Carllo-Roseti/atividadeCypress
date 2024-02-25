487a2cd35ff2c4db0b406d27a31feeec845d5187
jean carllo <jeanrosete04@gmail.com>
2024-02-25 15:18:49 -0300

    deixando o código com um contato original.

--- cypress/e2e/agendaContatos/listaDeContatos.cy.js
+++ cypress/e2e/agendaContatos/listaDeContatos.cy.js
@@ -1,20 +1,38 @@
 /// <reference types="cypress" />
 
 describe('Teste de função botões da lista de contatos', () => {
-
-    it('Teste o processo de Edição.', () => {
+    beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
         cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
-        cy.get('input[placeholder="Nome"]').should('have.value', 'gian Souza').clear()
-        cy.get('input[placeholder="E-mail"]').should('have.value', 'gian@ebac.com.br').clear()
-        cy.get('input[placeholder="Telefone"]').should('have.value', '11912345678').clear()
-        cy.get('input[placeholder="Nome"]').type('Ramon Oliveira')
-        cy.get('input[placeholder="E-mail"]').type('RamonOliveira@gmail.com.br')
-        cy.get('input[placeholder="Telefone"]').type('11912345471')
+    })
+
+    it('Teste o processo de Edição.', () => {
+        cy.get('input[placeholder="Nome"]').should('have.value', 'gian Souza')
+        cy.get('input[placeholder="E-mail"]').should('have.value', 'gian@ebac.com.br')
+        cy.get('input[placeholder="Telefone"]').should('have.value', '11912345678')
+
+    })
+    it('Teste Salvar', () => {
+        cy.get('input[placeholder="Nome"]').clear().type('Ramon Oliveira')
+        cy.get('input[placeholder="E-mail"]').clear().type('RamonOliveira@gmail.com.br')
+        cy.get('input[placeholder="Telefone"]').clear().type('11912345471')
         cy.get('.alterar').click()
         cy.get(':nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(1)').should('contain', 'Ramon Oliveira')
         cy.get(':nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(2)').should('contain', '11912345471')
         cy.get(':nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(3)').should('contain', 'RamonOliveira@gmail.com.br')
+    })
+
+    it('Teste do cancel.', () => {
+        cy.get('.cancelar').click()
+        cy.get('input[placeholder="Nome"]').should('have.value', '').clear()
+        cy.get('input[placeholder="E-mail"]').should('have.value', '').clear()
+        cy.get('input[placeholder="Telefone"]').should('have.value', '').clear()
+    })
+    it('Reinicio do teste', () => {
+        cy.get('input[placeholder="Nome"]').clear().type('gian Souza')
+        cy.get('input[placeholder="E-mail"]').clear().type('gian@ebac.com.br')
+        cy.get('input[placeholder="Telefone"]').clear().type('11912345678')
+        cy.get('.alterar').click()
     })
 })
 
