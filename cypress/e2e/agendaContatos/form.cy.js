23dbee45c6a09379b16f22157c38a8b9117cdfd4
jean carllo <jeanrosete04@gmail.com>
2024-02-28 17:37:57 -0300

    realizada a correção.

--- cypress/e2e/agendaContatos/form.cy.js
+++ cypress/e2e/agendaContatos/form.cy.js
@@ -9,16 +9,19 @@ describe('Teste de função adicioanr e Excluir', () => {
         cy.get('input[placeholder="Nome"]').type('Jorge Campos')
         cy.get('input[placeholder="E-mail"]').type('Jorge_Campos@gmail.com')
         cy.get('input[placeholder="Telefone"]').type('(13) 99411-2564')
-        cy.get('.adicionar').click()
-        cy.get('.sc-beqWaB.eQdhbg.contato').should('have.length', 4)
+        cy.get('.contato').then(contatos => {
+            const tamanhoInicial = contatos.length
+            cy.get('.adicionar').click()
+            cy.get('.contato').should('have.length', tamanhoInicial + 1)
+        })
     })
     
     it('Teste do botão Deletar', () => {
-        cy.get(':nth-child(3) > .sc-gueYoa > .delete').click()
-        cy.get('.sc-beqWaB.eQdhbg.contato').should('have.length', 3)
+            cy.get('.contato').then(contatos => {
+            const tamanhoFinal = contatos.length
+            cy.get(':nth-child(3) > .sc-gueYoa > .delete').click()
+            cy.get('.sc-beqWaB.eQdhbg.contato').should('have.length', tamanhoFinal - 1)
+        })
     })
 })
-
-
-
 
